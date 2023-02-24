f271e1d04090cc78654241a99badc32a656c0897
bengin34 <becaglar3434@gmail.com>
2023-02-24 23:09:37 +0300

    pages structure created

---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import ShowButton from '../components/ShowButton';
+
+const Main = () => {
+
+    return (
+        <div>
+        <Header />
+        <ShowButton />
+        </div>
+    )
+}
+
+export default Main
